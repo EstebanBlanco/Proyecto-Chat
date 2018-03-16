6889c7c630a6adf8470cbed7ebef29d836aa869a
EstebanBlanco <eblanco707@gmail.com>
2018-03-16 00:21:10 -0600

    Listo al parecer. Falta probar los 3

--- Cliente.py
+++ Cliente.py
@@ -1,46 +1,89 @@
 import socket
 import sys
-
 import select
+from thread import *
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-IP_address = raw_input("Digite el servidor a conectar: ")
-Port = int(input("Digite el puerto del servidor: "))
-
+IP_address = raw_input("IPv4 a conectar: ") #str(socket.gethostbyname(socket.gethostname()))
+Port = int(input("Puerto: "))
 server.connect((IP_address, Port))
 
+"""
+Entrada del nick al servidor.
+"""
 nick = raw_input("Por favor introduce tu nick > ")
-print("\n")
-
-# Enviamos el nick al server
 server.send(nick)
-# Recibimos la confirmacion o denegacion del server
 truck = server.recv(1)
-# Mientras sea 0 es decir lo deniege entramos en el loop hasta que reciba 1 es decir lo acepte
-while (truck == "0"):
+
+while truck == "0":
     nick = raw_input("\nEse nick ya esta en uso, por favor introduce otro > ")
     server.send(nick)
     truck = server.recv(1)
 
+def imprimirMenu():
+    print("\n--> Bienvenido al Servidor de Chat!!!\n"
+    "### Menu Servidor Chat ###\n"
+    "1: Obtener nombre\n"
+    "2: IPv4 del servidor\n"
+    "3: Cantidad de procesos\n"
+    "4: Consultar la hora\n"
+    "5: Entrar al chat\n")
+
+def hiloEscucha(conn, addr):
+    while True:
+        sockets_list = [conn]
+        read_sockets, write_socket, error_socket = select.select(sockets_list, [], [])
+        if read_sockets[0] == conn:
+            message = read_sockets[0].recv(2048)
+            print message
+
+def hiloEscribe(conn, addr):
+    estoyEnChat = False
+    while True:
+        if not estoyEnChat:
+            imprimirMenu()
+            message = raw_input(":::> ")
+        else:
+            message = raw_input()
+
+        server.send(message)
+        if message == "4":
+            message = raw_input(":::> ")
+            server.send(message)
+        if message == "5":
+            estoyEnChat = True
+        elif message == "salir":
+            estoyEnChat = False
+
+
+start_new_thread(hiloEscucha, (server, "listening"))
+
+start_new_thread(hiloEscribe, (server, "writing"))
+
+estoyEnChat = False
+
+"""
+    Flujo de conexion con el servidor.
+"""
 while True:
-    # Mantiene una lista de posible flujos de entrada
+    """
+    if not estoyEnChat:
+        imprimirMenu()
     sockets_list = [server]
-
-    """Hay dos posible situaciones de entrada. O el usuario quiere dar una entrada manual para
-    enviar a otras personas o el servidor esta enviando un mensaje para ser impreso en pantalla.
-    Selecciona las devoluciones de sockets_list, la stream que es el lector para la entrada.
-    Entonces, por ejemplo si el servidor quiere enviar un mensaje, entonces la condicion if
-    se mantendra verdadera. Si el usuario desea enviar un mensaje, la condicion se evaluara como 
-    verdadera """
     read_sockets, write_socket, error_socket = select.select(sockets_list, [], [])
-
     for socks in read_sockets + [sys.stdin]:
         if socks == server:
             message = socks.recv(2048)
             print message
         else:
-            message = raw_input("Digita tu mensaje > ")
+            message = raw_input(":::> ")
             server.send(message)
+            print("<Tu> " + str(message))
+            if message == "5":
+                estoyEnChat = True
+            elif message == "salir":
+                estoyEnChat = False
+    """
 
 server.close()
